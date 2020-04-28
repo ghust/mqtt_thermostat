92ebe6cfb52bbcebcb8a87e5723dd9a7aa09f009
Pieter Schaek <schaek.p@gmail.com>
2020-04-28 16:49:31 +0200

    OTA

--- mqtt_thermostat.ino
+++ mqtt_thermostat.ino
@@ -1,12 +1,12 @@
-
 #include <ArduinoJson.h>
-
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-
-
 #include <OpenTherm.h>
 #include <LinkedList.h>
+
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 class Thermostat {
   public:
@@ -188,6 +188,43 @@ void setup(void) {
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
 
+  //OTA
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname("MQTTThermostat");
+  
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+
 
 }
 
@@ -275,6 +312,7 @@ void reconnect() {
 
 
 void loop(void) {
+  ArduinoOTA.handle();
   float maxop = 0;
   new_ts = millis();
   i = i % 60; //antispam: Once every minute
